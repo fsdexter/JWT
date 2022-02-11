dd529c8f2b81464602d65e0762a387a287831995
fsdexter <fsdexter@gmail.com>
2022-02-11 19:26:58 +0000

    out<2

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +20,8 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/token',methods=['POST'])
+def create_token():
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
